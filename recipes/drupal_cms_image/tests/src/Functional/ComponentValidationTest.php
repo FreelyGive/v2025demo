52090398858a9532ecfc3e00389e655bea5c44a0
Marcus Johansson <me@marcusmailbox.com>
2025-09-25 08:41:10 +0200

    Update DCMS

--- recipes/drupal_cms_image/tests/src/Functional/ComponentValidationTest.php
+++ recipes/drupal_cms_image/tests/src/Functional/ComponentValidationTest.php
@@ -4,6 +4,9 @@ declare(strict_types=1);
 
 namespace Drupal\Tests\drupal_cms_image\Functional;
 
+use Drupal\breakpoint\BreakpointManagerInterface;
+use Drupal\Core\Entity\EntityTypeManagerInterface;
+use Drupal\eca\Entity\Eca;
 use Drupal\FunctionalTests\Core\Recipe\RecipeTestTrait;
 use Drupal\image\Entity\ImageStyle;
 use Drupal\Tests\BrowserTestBase;
@@ -45,6 +48,31 @@ class ComponentValidationTest extends BrowserTestBase {
         "The '$id' image style does not convert to '$expected_extension'.",
       );
     }
+
+    // Confirm that our configured breakpoints are exposed as plugins.
+    // @see \Drupal\drupal_cms_helper\Hook\PluginHooks::breakpointsAlter()
+    $manager = $this->container->get(BreakpointManagerInterface::class);
+    assert($manager instanceof BreakpointManagerInterface);
+    foreach (['sm', 'md', 'lg', 'xl'] as $key) {
+      $definition = $manager->createInstance("custom.$key")
+        ->getPluginDefinition();
+      $this->assertIsString($definition['label']);
+      $this->assertIsString($definition['mediaQuery']);
+      $this->assertIsInt($definition['weight']);
+      $this->assertSame(['1x'], $definition['multipliers']);
+      $this->assertSame('custom', $definition['group']);
+    }
+
+    // Deleting the ECA model should also delete the responsive image styles,
+    // because the config dependency is enforced.
+    $query = $this->container->get(EntityTypeManagerInterface::class)
+      ->getStorage('responsive_image_style')
+      ->getQuery()
+      ->accessCheck(FALSE)
+      ->count();
+    $this->assertSame(29, (int) $query->execute());
+    Eca::load('define_breakpoints')?->delete();
+    $this->assertSame(0, $query->execute());
   }
 
 }
