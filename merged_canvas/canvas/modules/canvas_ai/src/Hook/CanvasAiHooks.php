4e91e641ccd3568af373f59798f8dcff6035cf94
Marcus Johansson <me@marcusmailbox.com>
2025-09-29 12:06:14 +0200

    Fix picking components

--- merged_canvas/canvas/modules/canvas_ai/src/Hook/CanvasAiHooks.php
+++ merged_canvas/canvas/modules/canvas_ai/src/Hook/CanvasAiHooks.php
@@ -71,6 +71,10 @@ class CanvasAiHooks {
             'name' => $this->t('Menu Fetch Source'),
             'description' => $this->t('Returns the source for menu fetching.'),
           ],
+          'available_regions' => [
+            'name' => $this->t('Available Regions'),
+            'description' => $this->t('Returns the available regions in experience builder.'),
+          ],
           'json_api_module_status' => [
             'name' => $this->t('JSON API Module status'),
             'description' => $this->t('Returns the status of JSON API module.'),
@@ -124,6 +128,10 @@ class CanvasAiHooks {
 
           case 'menu_fetch_source':
             $replacements[$original] = !empty($data['menu_fetch_source']) ? $data['menu_fetch_source'] : NULL;
+            break;
+
+          case 'available_regions':
+            $replacements[$original] = !empty($data['available_regions']) ? $data['available_regions'] : NULL;
             break;
 
           case 'json_api_module_status':
--- prompt_examples/1-short-prompt_v02/1-short-prompt_v02.md
+++ prompt_examples/1-short-prompt_v02/1-short-prompt_v02.md
@@ -1,5 +1,5 @@
 Can you create a landing page for the Virtual FinTech Summit 2025 event?
 
-Make sure to have an eye catching hero section, some feature cards, a checklist with roles that benefit, a spotlight on our CEO and a strong CTA section with a link to register.
+Make sure to have an eye catching hero section, some feature cards, a checklist with roles that benefit and a strong CTA section with a link to register. Use image with graphics that represent fintech and technology, not photos.
 
 Make sure to get the date and important information about the event is written out, like the date.
